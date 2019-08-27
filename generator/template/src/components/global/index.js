cc4f1d562b07c074856e60c20dde39d36751557d
grasilife <grasilife@outlook.com>
2019-08-27 18:51:37 +0800

    lint

--- generator/template/src/components/global/index.js
+++ generator/template/src/components/global/index.js
@@ -1,10 +1,10 @@
 // 这里放置需要全局注册的组件
-import Vue from 'vue';
+import Vue from "vue";
 
 // 自动加载 global 目录下的 .vue 结尾的文件
-const componentsContext = require.context('./', true, /\.vue$/);
+const componentsContext = require.context("./", true, /\.vue$/);
 
-componentsContext.keys().forEach((component) => {
+componentsContext.keys().forEach(component => {
   const componentConfig = componentsContext(component);
   // 兼容 import export 和 require module.export 两种规范
   const ctrl = componentConfig.default || componentConfig;
