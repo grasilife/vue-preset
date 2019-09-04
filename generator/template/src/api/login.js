1733cb6af030ee5e015b0c0ecce23d99a9ac1aa6
grasilife <grasilife@outlook.com>
2019-09-04 15:46:23 +0800

    接口

--- generator/template/src/api/login.js
+++ generator/template/src/api/login.js
@@ -2,14 +2,14 @@ import services from "@/services/index";
 
 export function driverScreenInfo(data) {
   return services({
-    url: "/rest/common/deviceCommand/driverScreenInfo",
+    url: "/rest/common",
     method: "post",
     data
   });
 }
 export function getOnlineVehicleList() {
   return services({
-    url: "/rest/common/getOnlineVehicleList",
+    url: "/rest/common",
     method: "get"
   });
 }
