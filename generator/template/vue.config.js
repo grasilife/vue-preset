f5f7111277cf0455588a05778718a28552277351
grasilife <grasilife@outlook.com>
2019-11-21 18:46:24 +0800

    修改requireModuleExtension为true

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -90,7 +90,7 @@ module.exports = {
     // css预设器配置项
     loaderOptions: {},
     // 启用 CSS modules for all css / pre-processor files.
-    requireModuleExtension: false
+    requireModuleExtension: true
   },
   configureWebpack: () => ({
     name: "vue-cli3-template",
